5ba69baf29d9f2bee8dedd6d6b92e03a0764af13
sr08678 <sebastian.rauch@stud.th-deg.de>
2025-03-15 19:48:55 +0100

    asdf

---
+++ yoolink/static/js/script.js
@@ -0,0 +1,135 @@
+$(document).ready(function () {
+    const navbar = $('#top-info-bar')
+    let prevScrollPos = 0;
+
+    function updateNavbarState() {
+        // Get the new Value
+        const currentScrollPos = window.pageYOffset;
+    
+        if ((document.documentElement.scrollTop || document.body.scrollTop) === 0) {
+            // User has scrolled up to the top of the page
+            navbar.removeClass('scrolled');
+        } else {
+            navbar.addClass('scrolled');
+        }
+
+        // Update the old value
+        prevScrollPos = currentScrollPos;
+    }
+
+    window.addEventListener('scroll', updateNavbarState);
+
+
+    // Funktion zur Berechnung der Bannerhöhe und Zuweisung an die CSS-Variable
+    function setInfoBannerHeight() {
+        var bannerHeight = $('#info-banner').outerHeight() || 0;
+        $(':root').css('--info-banner-height', bannerHeight + 'px');
+    }
+
+    // Initiale Einstellung der Bannerhöhe
+    setInfoBannerHeight();
+
+    // Neu berechnen, falls Fenstergröße oder Inhalt sich ändert
+    $(window).on('resize', setInfoBannerHeight);
+
+
+
+    function openMenu() {
+        const menu = $('#mobile-menu');
+        menu.removeClass('hidden');
+        $('#menu-toggle2').removeClass("hidden");
+        // Füge kleine Verzögerung hinzu, damit Tailwind die Animation erkennt
+        setTimeout(() => {
+            menu.addClass('opacity-100').removeClass('opacity-0');
+        }, 10);
+    }
+    
+    function closeMenu() {
+        $('#menu-toggle2').addClass("hidden");
+        const menu = $('#mobile-menu');
+        menu.addClass('opacity-0').removeClass('opacity-100');
+    
+        // Warte auf die Animation, dann verstecke das Menü
+        setTimeout(() => {
+            menu.addClass('hidden');
+        }, 300); // 300ms = Tailwind transition duration
+        
+    }
+    
+    // Burger-Menü öffnen
+    $('#menu-toggle').on('click', function () {
+        openMenu();
+    });
+    
+    // Menü schließen
+    $('#menu-toggle2').on('click', function () {
+        closeMenu();
+    });
+    
+
+    // Banner anzeigen, wenn der Bildschirm auf Desktop-Größe erweitert wird
+    $(window).on('resize', function() {
+        if ($(window).width() >= 768) { // 768px ist die Grenze für 'md'
+            $('#info-banner').removeClass('hidden');
+            $('#mobile-menu').addClass('hidden');
+        }
+    });
+
+
+
+    $('.faq-question').click(function() {
+        // FAQ-Answer öffnen/schließen
+        $(this).next('.faq-answer').slideToggle();
+
+        // Toggle-Symbol ändern (+ / -)
+        var toggleSymbol = $(this).find('.faq-toggle');
+        toggleSymbol.text(toggleSymbol.text() === '+' ? '-' : '+');
+
+        // Alle anderen Antworten schließen
+        $('.faq-answer').not($(this).next('.faq-answer')).slideUp();
+        $('.faq-toggle').not(toggleSymbol).text('+');
+    });
+
+
+    // Swiper Slider
+    const swiper = new Swiper('.swiper', {
+        speed: 400,
+        spaceBetween: 100,
+        loop: true, // Aktiviert das endlose Swipen
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false, // Autoplay nach Benutzung der Buttons nicht deaktivieren
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true, // Bullets anklickbar machen
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+        },
+    });
+
+    const languageButton = $('#language-button');
+        const languageMenu = $('#language-menu');
+
+        languageButton.on('click', function () {
+            languageMenu.toggleClass('hidden');
+        });
+
+
+    const languageButton2 = $('#language-button2');
+        const languageMenu2 = $('#language-menu2');
+
+        languageButton2.on('click', function () {
+            languageMenu2.toggleClass('hidden');
+        });
+
+});
+
+
